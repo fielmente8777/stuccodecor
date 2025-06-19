bf64905cca9e765e2cced210021fc1ee704570a2
sumit <sk7627577@gmail.com>
2025-06-19 10:31:15 +0530

    Re-enable output configuration and adjust image settings in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export",
+  output: "export",
   images: {
-    // unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
-      },
-    ],
-    formats: ["image/avif", "image/webp"],
-    dangerouslyAllowSVG: true,
+    unoptimized: true,
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
+    //   },
+    // ],
+    // formats: ["image/avif", "image/webp"],
+    // dangerouslyAllowSVG: true,
   },
   trailingSlash: true,
 };
