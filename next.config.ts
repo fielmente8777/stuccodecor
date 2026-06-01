24dd7cb25ad621328def2c2baad3aec0db3cea6e
fielmente8777 <122347218+fielmente8777@users.noreply.github.com>
2026-06-01 22:17:51 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export",
+
+  poweredByHeader: false,
+
+  reactStrictMode: true,
+  
   images: {
 
     
@@ -11,10 +16,27 @@ const nextConfig: NextConfig = {
         hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
       },
     ],
-    // formats: ["image/avif", "image/webp"],
+    formats: ["image/avif", "image/webp"],
+
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+
+    minimumCacheTTL: 2678400,
+
     dangerouslyAllowSVG: true,
+
+    contentDispositionType: "attachment",
   },
   trailingSlash: true,
+
+   experimental: {
+    scrollRestoration: true,
+
+    optimisticClientCache: true,
+    optimizeCss: true,
+    optimizePackageImports: ["react-datepicker", "axios", "swiper/react"],
+  },
 };
 
 export default nextConfig;
